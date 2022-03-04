7303b4f57e890386343e18a5645455f170b6c83f
nhatcl <nhatcl@vnext.com.vn>
2022-03-04 15:47:52 +0700

    install openzeppelin, contract erc20

---
+++ contracts/gold/Gold.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Gold is ERC20 {
+    constructor() ERC20("GOLD", "GLD") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
