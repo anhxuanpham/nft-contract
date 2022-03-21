469670910d3e217d1979e26df8d99181b5563a49
nhatcl <nhatcl@vnext.com.vn>
2022-03-21 23:57:40 +0700

    update contract sample and unit test

--- test/bep20/test-sample-gold.js
+++ test/bep20/test-sample-gold.js
@@ -55,6 +55,7 @@ describe("ERC20-BEP20", function () {
             .transferFrom(accountA.address, accountC.address, amount)
             expect(await token.balanceOf(accountA.address)).to.be.equal(totalSupply - amount)
             expect(await token.balanceOf(accountC.address)).to.be.equal(amount)
+            expect(await token.allowance(accountA.address, accountB.address)).to.be.equal(0)
             await expect(transferFromTx).to.emit(token, 'Transfer').withArgs(accountA.address, accountC.address, amount)
             
         });
