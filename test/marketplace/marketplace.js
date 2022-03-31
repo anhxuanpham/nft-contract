cd40e72cc2764e3a730ecebb10fd34dd67fc4d01
nhatcl <nhatcl@vnext.com.vn>
2022-03-31 17:55:23 +0700

    marketplace and unit test layout

---
+++ test/marketplace/marketplace.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("marketplace", function () {
+    let [admin, seller, buyer, feeRecipient] = []
+    let petty
+    let gold
+    let marketplace
+    let defaulFeeRate = 10
+    let defaulFeeDecimal = 0
+    let defaulPrice = ethers.utils.parseEther("100")
+    let address0 = "0x0000000000000000000000000000000000000000"
+    let uri = "sampleuri.com/"
+    beforeEach(async () => {
+        [admin, seller, buyer, feeRecipient] = await ethers.getSigners();
+        const Petty = await ethers.getContractFactory("Petty");
+        petty = await Petty.deploy()
+        await petty.deployed()
+        const Gold = await ethers.getContractFactory("Gold");
+        gold = await Gold.deploy()
+        await gold.deployed()
+        const Marketplace = await ethers.getContractFactory("Marketplace");
+        marketplace = await Marketplace.deploy(petty.address, defaulFeeDecimal, defaulFeeRate, feeRecipient)
+        await gold.deployed()
+    })
+    describe("common", function () {
+        it("nft contract should return correct value", async function () {
+        });
+        it("feeDecimal should return correct value", async function () {
+        });
+        it("feeRate should return correct value", async function () {
+        });
+        it("feeRecipient should return correct value", async function () {
+        });
+    })
+    describe("updateFeeRecipient", function () {
+        it("should revert if feeRecipient is address 0", async function () {
+        });
+        it("should update correctly", async function () {
+        });
+    })
+    describe("updateFeeRate", function () {
+        it("should revert if fee rate >= 10^(feeDecimal+2)", async function () {
+        });
+        it("should update correctly", async function () {
+        });
+    })
+    describe("addPaymentToken", function () {
+        it("should revert paymentToken is Address 0", async function () {
+        });
+        it("should revert if address is already supported", async function () {
+        });
+        it("should revert if address is already supported", async function () {
+        });
+        it("should add payment token correctly", async function () {
+        });
+    })
+    //important
+    describe("addOrder", function () {
+        beforeEach(async () => {
+            petty.mint(seller.address)
+        })
+        it("should revert if payment token not supported", async function () {
+        });
+        it("should revert if sender isn't nft owner", async function () {
+        });
+        it("should revert if nft hasn't been approve for marketplace contract", async function () {
+        });
+        it("should revert if price = 0", async function () {
+        });
+        it("should add order correctly", async function () {
+        });
+    })
+    describe("cancelOrder", function () {
+        beforeEach(async () => {
+            petty.mint(seller.address)
+            petty.setApprovalForAll(marketplace.address, true)
+            marketplace.addOrder(1, gold.address, defaulPrice)
+        })
+        it("should revert if order has been sell", async function () {
+        });
+        it("should revert if sender isn't order owner", async function () {
+        });
+        it("should cancel correctly", async function () {
+        });
+    })
+    describe("executeOrder", function () {
+        beforeEach(async () => {
+            petty.mint(seller.address)
+            petty.setApprovalForAll(marketplace.address, true)
+            marketplace.addOrder(1, gold.address, defaulPrice)
+        })
+        it("should revert if sender is seller", async function () {
+        });
+        it("should revert if order has been sold", async function () {
+        });
+        it("should execute order correctly with default fee", async function () {
+        });
+        it("should execute order correctly with 0 fee", async function () {
+        });
+        it("should execute order correctly with fee 1", async function () {
+        });
+        it("should execute order correctly with fee 2", async function () {
+        });
+    })
+})
